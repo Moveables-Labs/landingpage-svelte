dc234e99ef6f5ec5f2ef55a647555d350c0601a7
Michaelcosj <michaelcosj@gmail.com>
2023-06-21 20:10:24 +0100

    initial commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,36 @@
+<nav>
+  <a href="/">
+    <div class="logo">
+      <img src="/logo.png" alt="logo" />
+      <img src="/logo-text.png" alt="logo-text" />
+    </div>
+  </a>
+  <ul class="nav-menu">
+    <li><a href="/">Home</a></li>
+    <li><a href="/">Socials</a></li>
+    <li><a href="/">About Us</a></li>
+    <li><button>Get Started</button></li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    padding: 0.5em 5em;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .logo {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+
+  .nav-menu {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+</style>
