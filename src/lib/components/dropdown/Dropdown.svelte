77b5fdaba9c38ed95d882812e6300a3413286103
Michaelcosj <michaelcosj@gmail.com>
2023-06-23 11:34:32 +0100

    basic dropdown implementation (styling sucks)

---
+++ src/lib/components/dropdown/Dropdown.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  let menu: HTMLUListElement;
+  export const toggle = () => {
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  };
+</script>
+
+<div>
+  <ul bind:this={menu}>
+    <slot />
+  </ul>
+</div>
+
+<style>
+  div {
+    display: flex;
+  }
+
+  ul {
+    list-style: none;
+    position: absolute;
+    background-color: white;
+    z-index: 100;
+    width: 5rem;
+    border-radius: 0.75rem;
+    padding: 3px 10px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    top: 4.5rem;
+    right: 24rem;
+  }
+</style>
