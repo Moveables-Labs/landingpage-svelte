9a8cd8db7ccba3dd939e44d36f3a446dbcc68bb5
danokundaye <dokundaye40@gmail.com>
2023-09-26 21:54:09 +0100

    web-testing

--- src/lib/components/Services.svelte
+++ src/lib/components/Services.svelte
@@ -94,16 +94,7 @@
       </div>
 
       <!-- Slider Previous and Next Buttons -->
-      <div class="slider-but">
-        <button class="prev" bind:this={prevBtn}
-          ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><style>svg{fill:#ffffff}</style><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
-          </button
-        >
-        <button class="next" bind:this={nextBtn}
-          ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><style>svg{fill:#ffffff}</style><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
-          </button
-        >
-      </div>
+
     </div>
   </div>
   <!--Web Slider ends here-->
@@ -111,7 +102,7 @@
 
   <div class="caro-sel">
     <div class="slider">
-      <div class="slides">
+      <div class="slides" bind:this={slides}>
         <div class="card-mob card-mob1">
           <div class="irr2">
             <img src="/images/Dispatch1-mol.png" alt="" />
@@ -172,19 +163,17 @@
           </div>
         </div>
       </div>
-
-      <!-- Slider Previous and Next Buttons -->
-      <div class="slider-but">
-        <button class="prev" bind:this={prevBtn}
-          ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><style>svg{fill:#ffffff}</style><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
-          </button
-        >
-        <button class="next" bind:this={nextBtn}
-          ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><style>svg{fill:#ffffff}</style><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
-          </button
-        >
-      </div>
     </div>
+  </div>
+  <div class="slider-but">
+    <button class="prev" bind:this={prevBtn}
+      ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><style>svg{fill:#ffffff}</style><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+      </button
+    >
+    <button class="next" bind:this={nextBtn}
+      ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><style>svg{fill:#ffffff}</style><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+      </button
+    >
   </div>
 </section>
 
